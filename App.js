b75912f412b32bb99b9d122fca5508442bae2444
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-05-20 09:27:48 -0500

    Examen 2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from "./Home";
 import Store from "./Store";
 import Details from "./Details";
@@ -10,11 +11,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Tienda" component={Store}/>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Tienda" component={Store}/>
         <Stack.Screen name="Details" component={Details} options={{title:"Objetos"}}/>
-      </Stack.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
